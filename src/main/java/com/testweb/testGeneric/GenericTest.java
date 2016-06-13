d15a6967b9d15d65331369bc883bf033a032bf36
xingxuechao <xchaos8@126.com>
2016-06-13 14:28:51 +0800

    add GenericTest

---
+++ src/main/java/com/testweb/testGeneric/GenericTest.java
@@ -0,0 +1,13 @@
+package com.testweb.testGeneric;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by xingxuechao on 2016/6/13.
+ */
+public class GenericTest {
+
+   // List<String>[] lsa = new ArrayList<String>[10];
+
+}
